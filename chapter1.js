2ccf27ab0b85aa938fb8e4c2b61395b13da4b494
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-09-09 22:28:51 +0530

    Add files via upload

---
+++ chapter1.js
@@ -0,0 +1,10 @@
+// // Values And Variables In JavaScript
+
+// var myname='My Name - Aniket Govind Kusundal';
+// var myage='my age is 19'
+// var address='Sector-9'
+
+
+// console.log(myname);
+// console.log(myage);
+// console.log(address);
