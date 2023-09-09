af89c2618390ef97634de9fd31931335c49aab50
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-09-09 22:29:10 +0530

    Add files via upload

---
+++ chapter2.js
@@ -0,0 +1,28 @@
+// Chapter 2 --> Data Types In Js
+
+
+
+// STRING DATA TYPE
+
+var myname = "Aniket Kusundal"
+console.log(myname)
+
+console.log(typeof (myname));
+
+
+
+// NUMBER DATA TYPE
+
+var myage = 19;
+console.log(myage)
+console.log(typeof (myage))
+
+
+//  BOOLEAN DATA-TYPE
+var iAmAniket = true;
+console.log(iAmAniket)
+console.log(typeof (iAmAniket))
+var iAmAniket = false;
+console.log(iAmAniket)
+console.log(typeof (iAmAniket))
+
