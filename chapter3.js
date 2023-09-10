c08d1ea6062fd16ce4ba4f4ab4847369744cf8d2
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-09-10 14:48:59 +0530

    Add files via upload

---
+++ chapter3.js
@@ -0,0 +1,276 @@
+       
+       
+       
+//  EXPRESSION AND OPRATORS
+
+
+
+
+
+        //         (1)    Assigenment Oprator
+  
+//  var x = 5
+//  var y = 5
+
+//  console.log("is both the X and Y are Equal or Not" + x==y)           ----> It Is False
+
+// console.log(x==y)             -----> It Is true
+
+
+// console.log('It Both The X and Y are equal ': ${ x==y });
+// --->  It Both The X and Y are equal ': true   ------> it is true
+
+
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\
+
+
+
+
+
+//         (2)           ARITHMATIC OPRATOR
+
+
+//  An arithmatic oprator Take Numerical Value
+// (Either literrals or Variables) As their Oprands and returns a Single Numerical Value
+
+
+// console.log(30+20);
+// console.log(85-55);
+// console.log(24*7);
+// console.log(20/5);
+
+// console.log("reminder oprator"+ 85%8);
+
+
+
+
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\
+
+
+
+
+
+
+
+
+
+
+//  INCREMENT AND DECREMENT OPRATOR
+
+      
+
+
+    // var Num=15;
+    // var Newnum=Num++;
+    //   console.log(Num);
+    // console.log(Newnum);
+
+
+// 1
+//     var Num=15;
+//     var Newnum=Num++ +5;
+//       console.log(Num);
+//     console.log(Newnum);
+
+// // 2
+//     var Num=15;
+//     var Newnum= ++Num +5;
+//       console.log(Num);
+//     console.log(Newnum);
+    
+
+
+    // Postfix oprator means the expression is evaluted the first using the orignal value of the variable is increment(incressed)  
+
+
+    //  if used prefix with oprator before oprand (For Example)  ++X )
+
+    //  The Increment Oprator Increments And return The VAlue after the incrementing
+
+
+    // If used Prefix With oprator (For example ++X)
+
+    // Increment Oprator Means The Variable Is increment First And Then The Expression is evaluted Using The NEw Value Of The Variable
+
+
+
+
+
+
+
+
+    // PREFIX DECREMENT OPRATOR
+
+
+    // 1
+  //   var Num=15;
+  //    var Newnum=Num-- +5;
+  //      console.log(Num);
+  //  console.log(Newnum);
+
+
+  // //   2
+  //  var Num=15;
+  // var Newnum= --Num +5;
+  //     console.log(Num);
+  //   console.log(Newnum);
+
+
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
+
+//         (3)    COMPARISION OPRATOR
+    
+
+
+// ----->   A Comparision Oprator Compares Its Opreand And Returns A logical Value Based On Whatever The Comparision Is True
+
+
+
+
+//  [1]. {==}
+// var a = 30;
+// var b = 20;
+ 
+//   console.log(a==b);
+
+
+// [2]. {=/=}
+
+// var a = 30;
+//  var b = 20;
+
+//  console.log(a!=b);
+
+
+//  [3]. Grater Than  (>)
+
+//  var a=30;
+//  var b=20;
+
+//  console.log(a>b);
+
+
+
+// [4]. Grater Than Or Equal {>=}
+     
+    // var a = 30;
+    // var b = 20;
+    // console.log(a>=b);
+
+
+
+
+    // [5].   Less Than (<)
+
+        // var a = 30;
+        // var b = 20;
+        // console.log(a<b);
+
+
+
+        //  [6]. Less Than Or Equal
+
+          //  var a = 30;
+          //  var b = 20;
+          //  console.log(a<=b);
+
+
+
+
+
+
+
+          // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\
+
+
+
+
+
+
+          //  LOGICAL OPRATOR..............
+
+
+          //  Logical Oprator Typically Used With Boolean (Logical Value)
+          //  When They Are They a return A Boolean VAlue
+          
+          
+          // var a = 30;
+          //  var b = -20;
+
+
+          // LOGICAL AND OPRATOR(&&)
+           
+          // the logical AND oprator for a set of oprand is true if and only All of Its oprands/expression are true
+
+
+          // csole.log(a>b && b>0);on
+          // console.log(a>b && b<a);
+          // console.log(a>b && b>0 && b<0);
+          // console.log(a>b && b>-50 && b<0);
+
+
+
+
+          // LOGICAL OR OPRATOR
+            
+
+          // var a = 30;
+          // var b = -20;
+
+          // console.log( (a > b) || (b>-50) || (b>0) );  -----> It is true
+          // console.log(( a < b ) || ( b > 0 ) ||  (b > 0) );
+
+
+
+
+
+//   LOGICAL NOT OPRATOR (!)
+
+    // The Logical NOT (!) OPrator (LOGICAL COMPLEMENT NEGATION)
+    // Takes TRUTH to Falsily And Vice Varsa
+
+
+    // var a = 30;
+    // var b = -20;
+
+    // true ko false or false ko true karta hain
+
+    // console.log(!((a>b) || (b<0)));
+
+    // console.log(!false);
+    // console.log(!true);
+
+
+
+
+
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\
+
+
+
+
+//  [5]. STRING CONCATENTION OPRATOR........
+
+
+// The concatention oprator (+) concantentions of two string value togather 
+// returning Another String That Is The Union Of  The Two Sting
+
+console.log("ANIKET " + "KUSUNDAL");
+
+var myname= "Aniket"
+
+console.log(myname +" Kusundal");
+
+
+
+
+
+
+
