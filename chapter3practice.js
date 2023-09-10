3fff13dc6b13ed6e29a3eddb3a80c8d791c4d499
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-09-10 14:49:15 +0530

    Add files via upload

---
+++ chapter3practice.js
@@ -0,0 +1,87 @@
+
+// CHALLENGE NO. 4 PREACTICESET [4]
+
+// Q.1 What Will Be The Output of 3**3 ?
+
+// ---->  
+
+// console.log(3**3);   3*3*3
+//    O/P ---> 27
+
+
+// Q.2  What will be the output when we add a number and a string
+
+//    ---->   
+// console.log( 5 + "Aniket");
+
+
+
+// Q.3  write a program to SWAP Numbers ?
+ 
+// ---->
+// var a=30;
+// var b=10;
+
+// c=b;
+// b=a;
+// a=c;
+
+// console.log("The value of A is " + a);
+// console.log("The value of B is " + b);
+
+
+
+
+
+
+
+//  Q.4 Write a program to swap two numbers Without Using The Variable
+
+//  ---->
+     
+// var a=50;
+// var b=40;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log("The value of A is " + a);
+//  console.log("The value of B is " + b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  INTERVIEW QUESTION
+
+
+//  DIFFRENCE B/W (==) & (===)
+
+// # (==)
+   
+//  var num1 = 5;
+//  var num2 = '5';
+
+//    console.log(typeof(num1));
+//    console.log(typeof(num2));
+//    console.log(num1 == num2);
+
+
+// # (===)
+
+// var num1 = 5;
+//  var num2 = '5';
+
+//    console.log(typeof(num1));
+//    console.log(typeof(num2));
+//    console.log(num1 === num2);
