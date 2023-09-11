767e0eb60b04bfefbb677826d8af4db0bdc94a63
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-09-11 22:59:55 +0530

    Add files via upload

---
+++ chapter5.js
@@ -0,0 +1,79 @@
+//  FUNCTIONS IN JAVASCRIPT
+
+// [1]   FUNCTION DEFINATUION 
+
+// function sum() {
+//     var a = 10, b = 30;
+//     var total = a + b;
+//     console.log(total);
+    
+// }
+
+// sum();                ------>  FUNCTION CALL   [2]
+
+
+
+
+// [3] FUNCTION PARAMETERS    VS       FUNCTION ARGUMENTS  .
+
+
+// function sum(a,b){
+//     var total = a + b;
+//     console.log(total);
+// }
+
+// sum(999,1)
+// sum(599,401)
+
+
+
+
+
+
+// [4].   FUNCTION EXPRESSION....
+
+// function expression Simply means create a function And PUT it into a the Variable
+
+
+// function sum(a,b){
+//     var total = a + b;
+//     console.log(total);
+// }
+
+
+// var funExp = sum (6,4);
+
+
+
+
+//  [5].  RETURN KEYWORD   :---->
+
+// When JavaScript reaches a return statement the function will stop executing
+// A Function often compute a return value the return value "Returned" Back to the "CALLER"
+
+
+
+// function sum(a,b){
+//     return total = a+b;
+// }
+// var funExp = sum (5, 25)
+// console.log(funExp);
+
+
+
+
+//  [6]. ANONYMOUS FUNCTION 
+
+// 1.  The function expression similar to and has the same syntax as a function declaration one can define "NAMED"
+
+// 2.  function expression (Where the name of the expression might be used in the call stack for example or "ANONYMOUS " function expresssion)
+
+// var funExp = function( a , b){
+//     return total = a + b ;
+
+// }
+// var sum = funExp(40 , 60 )
+// console.log("the sum of two number is "+ sum);
+
+
+
