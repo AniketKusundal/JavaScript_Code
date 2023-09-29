c9197193dc6cd1d19c246a776424d7dba4bd55bb
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-09-29 21:15:47 +0530

    Add files via upload

---
+++ chapter11.js
@@ -0,0 +1,102 @@
+// ====>>  SECTION - 9 -->  MATH OBJECT IN JAVASCRIPT  <=====  \\
+
+//  --> The Math Object in javaScript allows you to Perfrom mathmatical task on Numbers 
+
+
+
+// console.log(Math.PI);
+
+
+// [1]. MATH.ROUND ()
+// -->  Returns The Value of X Rounded to its nearest interger 
+
+// let num = 10.5334
+// console.log(Math.round(num));
+
+
+// [2].  MATH.POW()
+// ----> math.pow (X , Y) returns the value of X of The Power of Y.
+
+    // console.log(Math.pow(2,3));
+
+    // console.log(2**3);
+//  NOTE:- BOTH ARE SAME 
+
+
+// [3]. MATH.SQRT()
+
+// --->  math.sqrt returns the returns the square root of X
+
+// console.log(Math.sqrt(20));
+// console.log(Math.sqrt(23));
+// console.log(Math.sqrt(23));
+
+
+
+// [4]. MATH.ABS()
+
+// -->  math.abs() returns the absolute (positive value of X);
+// -->  It converts negative to positive 
+
+// console.log(Math.abs(-223));
+// console.log(Math.abs(524));
+
+
+// [5].  MATH.CEIL() 
+
+// --->  Math.Ceil(X) returns the value of X rounded upto its Nearest Intergerr 
+
+// console.log(Math.ceil(3.2));
+// console.log(Math.ceil(2));
+
+// [6].  MATH.MIN()
+
+// --->  Math.min can be to find a lowest value in a list of argument 
+
+// console.log(Math.min(0,51,800,550,5555,10));
+
+// [7].  MATH.MAX().
+
+// --->  Math.max can be to find a largest value in a list of argument
+
+// console.log(Math.max(0,51,800,550,5555,10));
+
+
+// [8]. MATH.FLOOR()
+// --->  the math floor static method always round downs and returns the largest interger lesss than or equal to a given number.
+
+// console.log(Math.floor(4.6));
+
+
+
+// [9] MATH.RANDOM()
+// ----> Math.random() returns the randomm number between 0 (Inclusive) , and 1 (Exclusive).
+
+            // ----> MAth.randomm always returns the anumber lower than  1
+
+
+// console.log(Math.random()*10);
+
+// console.log(Math.random(1*10));
+
+
+
+
+// [10]. Math.turnc ()  --> truncate
+
+//  -->  the Math.turnc() method returns the interger part of a number 
+
+// console.log(Math.trunc(4.6));
+// console.log(Math.trunc(-99.1));
+
+
+
+
+
+
+
+//  Practice Time
+
+
+//  ---> If the Argument is a positive number Math.turnc() is Equivlant to math.floor() 
+// Otherwise Math.turnc() is Equivilant to Math.Ceil()
