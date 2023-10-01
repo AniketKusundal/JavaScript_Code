0eba07899002a8c56be050ad60811ec7930c70dd
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-01 11:50:12 +0530

    Add files via upload

---
+++ chapter13.js
@@ -0,0 +1,85 @@
+//  SECTION ==>  11     =====>> EVENTS IN JAVASCRIPT  <<======   \\
+
+// HTML Events
+
+// An HTML event can be something the browser does, or something a user does.
+
+// Here are some examples of HTML events:
+
+//     An HTML web page has finished loading
+//     An HTML input field was changed
+//     An HTML button was clicked
+
+// Often, when events happen, you may want to do something.
+
+// JavaScript lets you execute code when events are detected.
+
+// HTML allows event handler attributes, with JavaScript code, to be added to HTML elements.
+
+//  What will we do in Event Secction
+
+// 1)  four ways to writing events in JS
+
+// 2) what is event Object
+
+// 3) mouse events in javasccript
+
+// 4) input events in javascript
+
+//               sectiion - 1)  four ways to writing events in JS        //
+
+
+// 1) using inline event alert();
+
+// 2) by calling a function (we already seen most comman way to writing)
+
+// 3) using inline events (HTML onclick="property and element. onclick")
+
+// 4) using addEventListener(addEventListener and ie's attach event)
+
+
+
+
+
+//  2nd way of writing function 
+
+// const callingfunction=()=>{
+//     alert('Most comman way to writing function');
+// }
+
+
+
+
+//   3rd way of writing   funtion   
+
+// see  this content in DOM BOM AND EVENTS IN JAVASCRIPT this file 
+
+
+
+
+
+
+
+
+
+// <!-- Mouse Events  Event 	Occurs When -->
+
+// <!-- onclick                      	A user clicks on an element
+
+// 1  oncontextmenu 	                A user right-clicks on an element
+
+// 2  ondblclick 	                    A user double-clicks on an element
+
+// 3  onmousedown 	                A mouse button is pressed over an element
+
+// 4  onmouseenter 	                The mouse pointer moves into an element
+
+// 5  onmouseleave 	                he mouse pointer moves out of an element
+
+// 6  onmousemove 	                The mouse pointer moves over an element
+
+// 7  onmouseout 	                    The mouse pointer moves out of an element
+
+// 8  onmouseover 	                The mouse pointer moves onto an element
+
+// 9  onmouseup 	                    A mouse button is released over an element -->
