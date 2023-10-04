50071e0158247cf8d9b09d2e323ce5b01f09f0ab
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-04 21:59:46 +0530

    Add files via upload

---
+++ nan.js
@@ -0,0 +1,39 @@
+// WHAT IS NAN ?
+
+//   --> NAN Is The Property Of Globel Object
+//   --> In Another Words It Is A Variabel In Globel Space
+//   --> The Initial Value Of NAN is NOT-A-NUMBER
+
+
+
+  /*  var myphonenumber ="3263636363";
+  var myname = "Aniket";
+
+    console.log(isNaN(myphonenumber))
+  console.log(isNaN(myname))
+
+
+      if(isNaN(myphonenumber)){
+         console.log("Enter Valid Phone Number");
+      }
+
+      if(isNaN(myname)){
+
+         console.log("Enter Valid Phone Number");
+      }  */
+      
+
+
+
+
+
+    //   PRACTICE NAN
+        
+     
+    //  (1) NaN === Nan;
+    //  (2) Number.NaN===Nan;
+    //  (3) isNaN(NaN);
+    //  (4) Number.isNaN(NaN);
+
+
+    console.log(Number.isNaN(NaN));
