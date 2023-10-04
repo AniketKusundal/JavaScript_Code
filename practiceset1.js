f1c2e4bcffc8cdd449c26313f59675b9c67f64c5
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-04 21:59:25 +0530

    Add files via upload

---
+++ practiceset1.js
@@ -0,0 +1,35 @@
+// Chapter 1 challange/practiceSet --->1 Values And Variables
+
+
+//   Example number 1
+
+//  var _myname="Aniket"
+
+//  console.log(_myname)
+// --> This Is The Valid declaration
+
+
+// Example Number 2
+
+//    var _1my__name="aniket";
+
+//     console.log(_1my__name)
+    // --> This Is The Valid Declaration
+
+
+
+    // Examle number 3
+
+    //    var 1myname="Aniket";
+    //    console.log(1myname)
+
+    //    --> This is Wrong Declaration
+
+
+
+    // Example Number 4
+     
+    //  var $myname="Aniket Kusundal";
+    //  console.log($myname)
+
+    //  --> This Is The Valid Declaration
