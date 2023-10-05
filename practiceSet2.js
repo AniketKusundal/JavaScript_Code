d38b79d1313cc5fc0158b3a8c84f5125784669f9
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-05 23:27:24 +0530

    Add files via upload

---
+++ practiceSet2.js
@@ -0,0 +1,52 @@
+// Challenge Time --> Practice Set 2----------- DATA-TYPE PRACTICE
+
+
+//  Example --> 1
+    (10+ "20")
+    console.log(10+ "20");
+
+    // Example --> 2
+    19-"9"
+     console.log(19-"9");   /*This is the BUG */
+
+
+    // Example --> 3
+
+    "Java" + "Script"
+    console.log("Java"+ "Script");
+    
+
+    // Example --> 4
+    " " + 0
+    console.log(" " + 0);
+
+
+    // Example --> 5
+
+    "Aniket" - "Kusundal"
+    console.log("Aniket" - "Kusundal");
+
+    // Example --> 6
+
+    " " +" "
+    console.log(" " +" ")
+
+
+    // Example --> 7
+    true + true
+    console.log(true + true)
+
+
+    // Example --> 8
+    true  + false
+    console.log( true  + false)
+
+    // Example --> 9
+    false + true
+    console.log( false + true)
+
+
+
+//  Example --> 10
+       false - true
+       console.log(false - true)
