6c9ce7c7d374cf246619895be9edd0b92ea92113
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-05 23:28:12 +0530

    Add files via upload

---
+++ practiceset4.js
@@ -0,0 +1,11 @@
+//  Practice of FOR LOOP
+
+
+//  * Write a javascript Program To print Table of Given Number (8,9,12,15) Using FOR LOOP   ?
+
+// for (var num = 1; num <= 10; num++);
+// debugger;
+// {
+//     tableOf = 8;
+//     console.log( tableOf * num);
+// }
